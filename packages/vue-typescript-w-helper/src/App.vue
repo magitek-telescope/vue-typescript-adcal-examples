b01beae5f6e1b654087118f960e7144348947b58
HANATANI Takuma <mail@potato4d.me>
2018-12-12 12:27:09 +0900

    refactor: format with prettier

--- packages/vue-typescript-w-helper/src/App.vue
+++ packages/vue-typescript-w-helper/src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Vue from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default Vue.extend({
   name: 'app',
   components: {
     HelloWorld
   }
-});
+})
 </script>
 
 <style>
